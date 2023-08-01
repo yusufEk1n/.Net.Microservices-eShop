97730d3f2c141903f5e0094d47eb19ac53878961
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-08-01 23:49:26 +0300

    MongoDB connection is ready to product data

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            // To do: Add seed data
+            // CatalogContextSeed.SeedData(Products); 
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
