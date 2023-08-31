7ab5e2f3e239bb5b9f2a280ab3c934b29e49953c
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-09-01 00:01:09 +0300

    Added common folder for domain layer

---
+++ Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.SeedWork
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
