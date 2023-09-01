9ed6a0c535fb2e8220608c4fb4f31b427b4f0561
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-09-01 20:50:08 +0300

    Created GetOrdersList Query

---
+++ Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<Order>>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentException(nameof(orderRepository));
+        }
+
+        public async Task<List<Order>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrderByUserNameAsync(request.UserName);
+            return orders.ToList();
+        }
+    }
+}
