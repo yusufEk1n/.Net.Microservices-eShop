86f41d5aa81f26941bf6aaaa1710c42a8b547723
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-09-02 16:46:53 +0300

    Created DbContext

---
+++ Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+        {
+            if (context.Orders.Any())
+            {
+                context.Orders.AddRange(GetPreConfiguredOrders());
+                await context.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbcontextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() { UserName = "kmn", FirstName = "Yusuf", LastName = "Ekin", EmailAdress = "yusuf@gmail.com", AdressLine = "Istanbul", Country = "Turkey", TotalPrice = 350 }
+            };
+        }
+    }
+}
