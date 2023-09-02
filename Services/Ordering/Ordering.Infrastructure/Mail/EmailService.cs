b9153e2ad052b5f077a8e9b0cbb936940569e972
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-09-02 22:41:00 +0300

    Added email service

---
+++ Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSetting _emailSetting;
+        private readonly ILogger<EmailService> _logger;
+
+        public EmailService(IOptions<EmailSetting> emailSetting, ILogger<EmailService> logger)
+        {
+            _emailSetting = emailSetting.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmailAsync(Email email)
+        {
+            var client = new SendGridClient(_emailSetting.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSetting.FromAdress,
+                Name = _emailSetting.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent.");
+
+            if (response.StatusCode == HttpStatusCode.Accepted || response.StatusCode == HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("Email sending failed.");
+
+            return false;
+        }
+    }
+}
